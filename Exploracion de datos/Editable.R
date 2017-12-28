db0b4383d3012efe2349a94a0bc9dd65e392888c
attearturo <gomezardi@hotmail.com>
2017-12-28 14:27:14 -0500

    Fisrt commit

---
+++ Exploracion de datos/Editable.R
@@ -0,0 +1,17 @@
+install.packages("ggplot2")
+install.packages("knitr")
+
+library(ggplot2)
+library(knitr)
+
+str(diamonds)
+
+//Diagrama de dispersion con Precio y Peso como ejes
+grafico <- ggplot(diamonds, aes(x=diamonds$carat, y=diamonds$price))
+grafico <- grafico + geom_point(aes(color = diamonds$clarity, alpha = 0.5))
+grafico
+
+//Diagrama de dispersion con Precio y Peso como ejes, facetas
+grafico <- ggplot(diamonds, aes(x=diamonds$carat, y=diamonds$price))
+grafico <- grafico + labs(x="Peso", y="Precio", title="Comercio de Diamantes") + facet_wrap(~ diamonds$color)+ geom_point(aes(color = diamonds$color, alpha = 0.5))
+grafico
